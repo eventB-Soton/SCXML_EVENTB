51a77e1fecbaf721d83b9a7ebc7810a804cda304
cfsnook <cfs@ecs.soton.ac.uk>
2018-02-11 21:28:06 +0000

    fix problems with final state translation and elaboration of outgoing

--- ac.soton.scxml.eventb/src/ac/soton/scxml/eventb/rules/ScxmlFinalType2FinalRule.java
+++ ac.soton.scxml.eventb/src/ac/soton/scxml/eventb/rules/ScxmlFinalType2FinalRule.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.sirius.tests.sample.scxml.ScxmlFinalType;
 import org.eclipse.sirius.tests.sample.scxml.ScxmlPackage;
@@ -25,13 +26,32 @@ import org.eventb.emf.core.machine.MachinePackage;
 import ac.soton.emf.translator.TranslationDescriptor;
 import ac.soton.emf.translator.configuration.IRule;
 import ac.soton.emf.translator.utils.Find;
+import ac.soton.eventb.statemachines.AbstractNode;
+import ac.soton.eventb.statemachines.Final;
 import ac.soton.eventb.statemachines.State;
 import ac.soton.eventb.statemachines.Statemachine;
 import ac.soton.eventb.statemachines.StatemachinesPackage;
+import ac.soton.eventb.statemachines.Transition;
 import ac.soton.scxml.eventb.utils.Make;
 import ac.soton.scxml.eventb.utils.Utils;
 
-public class ScxmlFinalTypeRule extends AbstractSCXMLImporterRule implements IRule {
+
+/**
+ * This rule generates, from an Scxml final state,
+ * the final iUML-B node and the final transition that targets it.
+ * 
+ * The pre-final state, also generated from an Scxml final state,
+ * is the source of the final transitions and so
+ * must already have been generated (is a dependancy)
+ * (see rule ScxmlFinalTypeRule) 
+ * 
+ * This rule needs to run late so that it can find the events that are elaborated 
+ * by outgoing transitions of the parent state.. the final transition also elaborates them.
+ * 
+ * @author cfs
+ *
+ */
+public class ScxmlFinalType2FinalRule extends AbstractSCXMLImporterRule implements IRule {
 
 
 	ScxmlScxmlType scxmlContainer=null;
@@ -53,7 +73,7 @@ public class ScxmlFinalTypeRule extends AbstractSCXMLImporterRule implements IRu
 	 */
 	@Override
 	public boolean dependenciesOK(EObject sourceElement, final List<TranslationDescriptor> translatedElements) throws Exception  {
-		((ScxmlFinalType) sourceElement).getId();
+		String finalName = ((ScxmlFinalType) sourceElement).getId();
 		String parentSmName = (stateContainer==null? scxmlContainer.getName() : stateContainer.getId())+"_sm";
 		statemachines.clear();
 		int refinementLevel = Utils.getRefinementLevel(sourceElement.eContainer());
@@ -62,14 +82,29 @@ public class ScxmlFinalTypeRule extends AbstractSCXMLImporterRule implements IRu
 			Machine m = (Machine) Find.translatedElement(translatedElements, null, null, MachinePackage.Literals.MACHINE, Utils.getMachineName(scxmlContainer,i));
 			Statemachine sm = (Statemachine) Find.element(m, null, null, StatemachinesPackage.Literals.STATEMACHINE, parentSmName);
 			if (sm==null) return false;
-			statemachines.add(sm);			
+			//check that the 'pre-final' state has already been generated from the ScxmlFinal
+			statemachines.add(sm);
+			if (findState(sm, finalName)==null) return false; 
 		}
 		return true;
 	}
 	
+	private State findState(Statemachine sm, String finalName) {
+		if (finalName==null) return null;
+		for (AbstractNode nd : sm.getNodes()){
+			if (nd instanceof State && finalName.equals(nd.getName())){
+				return (State) nd;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * fire late to make sure that the parent state has its outgoing transitions
+	 */
 	@Override
 	public boolean fireLate(){
-		return false;
+		return true;
 	}
 	
 
@@ -77,21 +112,24 @@ public class ScxmlFinalTypeRule extends AbstractSCXMLImporterRule implements IRu
 	public List<TranslationDescriptor> fire(EObject sourceElement, List<TranslationDescriptor> generatedElements) throws Exception {
 
 		ScxmlFinalType scxmlFinal = (ScxmlFinalType)sourceElement;
-
-		State state = null;
 		
 		for (Statemachine sm : statemachines){
-			if (state==null){
-				state = (State)Make.state(scxmlFinal.getId(), "");
-			}else{
-				state = (State) Utils.refine(scxmlContainer, state);
-			}
-
-			sm.getNodes().add(state);
 			
+			State preFinalState = findState(sm, scxmlFinal.getId());
+			
+			Final finals = (Final)Make.finalState(sm.getName()+"_final");
+			sm.getNodes().add(finals);
+		
+			Transition tr = (Transition)Make.transition(preFinalState, finals, "");
+			sm.getTransitions().add(tr);
+			
+			State parentState = (State) (sm.eContainer() instanceof State? sm.eContainer() : null);
+			EList<Transition> outgoing = parentState.getOutgoing();
+			for (Transition out : outgoing){
+				tr.getElaborates().addAll(out.getElaborates());
+			}
 		}
 		return Collections.emptyList();
 	}
-
 
 }
