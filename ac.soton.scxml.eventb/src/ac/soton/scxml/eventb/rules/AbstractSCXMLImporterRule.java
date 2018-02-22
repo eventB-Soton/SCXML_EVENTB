4702fa60429ed6a392192ec25651d833c17467c2
cfsnook <cfs@ecs.soton.ac.uk>
2018-02-22 22:13:25 +0000

    extend AbstractEventBGeneratorRule

--- ac.soton.scxml.eventb/src/ac/soton/scxml/eventb/rules/AbstractSCXMLImporterRule.java
+++ ac.soton.scxml.eventb/src/ac/soton/scxml/eventb/rules/AbstractSCXMLImporterRule.java
@@ -12,18 +12,14 @@ package ac.soton.scxml.eventb.rules;
 
 import java.util.List;
 
-import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.sirius.tests.sample.scxml.ScxmlPackage;
 import org.eclipse.sirius.tests.sample.scxml.ScxmlScxmlType;
-import org.eventb.emf.core.CorePackage;
-import org.eventb.emf.core.context.ContextPackage;
-import org.eventb.emf.core.machine.MachinePackage;
 
-import ac.soton.emf.translator.configuration.AbstractRule;
 import ac.soton.emf.translator.configuration.IRule;
+import ac.soton.emf.translator.eventb.rules.AbstractEventBGeneratorRule;
 import ac.soton.emf.translator.utils.Find;
 import ac.soton.eventb.emf.core.extension.coreextension.CoreextensionPackage;
 import ac.soton.eventb.statemachines.StatemachinesPackage;
@@ -37,25 +33,8 @@ import ac.soton.scxml.eventb.utils.IumlbScxmlAdapter;
  * @author cfs
  *
  */
-public abstract class AbstractSCXMLImporterRule extends AbstractRule implements IRule {
+public abstract class AbstractSCXMLImporterRule extends AbstractEventBGeneratorRule implements IRule {
 
-	protected static final EReference components = CorePackage.Literals.PROJECT__COMPONENTS;
-	protected static final EReference sees = MachinePackage.Literals.MACHINE__SEES;
-	protected static final EAttribute seesNames = MachinePackage.Literals.MACHINE__SEES_NAMES;
-	protected static final EAttribute refinesNames = MachinePackage.Literals.EVENT__REFINES_NAMES;
-	protected static final EReference variables = MachinePackage.Literals.MACHINE__VARIABLES;
-	protected static final EReference invariants = MachinePackage.Literals.MACHINE__INVARIANTS;
-	protected static final EReference events = MachinePackage.Literals.MACHINE__EVENTS;
-	protected static final EReference parameters = MachinePackage.Literals.EVENT__PARAMETERS;
-	protected static final EReference witnesses = MachinePackage.Literals.EVENT__WITNESSES;
-	protected static final EReference guards = MachinePackage.Literals.EVENT__GUARDS;
-	protected static final EReference actions = MachinePackage.Literals.EVENT__ACTIONS;
-	protected static final EReference sets = ContextPackage.Literals.CONTEXT__SETS;
-	protected static final EReference constants = ContextPackage.Literals.CONTEXT__CONSTANTS;
-	protected static final EReference axioms = ContextPackage.Literals.CONTEXT__AXIOMS;
-	protected static final EReference _extends = ContextPackage.Literals.CONTEXT__EXTENDS;
-	protected static final EAttribute extendsNames = ContextPackage.Literals.CONTEXT__EXTENDS_NAMES;
-	protected static final EReference extensions = CorePackage.Literals.EVENT_BELEMENT__EXTENSIONS;
 	protected static final EReference nodes = StatemachinesPackage.Literals.STATEMACHINE__NODES;
 	protected static final EReference statemachines = StatemachinesPackage.Literals.STATEMACHINE_OWNER__STATEMACHINES;
 	protected static final EReference transitions = StatemachinesPackage.Literals.STATEMACHINE__TRANSITIONS;
@@ -67,15 +46,13 @@ public abstract class AbstractSCXMLImporterRule extends AbstractRule implements 
 	
 	protected static final TranslationKind tkind = TranslationKind.MULTIVAR;
 
-	protected ITranslatorStorage storage = TranslatorStorage.getDefault();
-
 	
 	/**
 	 * This finds the refinement depth required by the Scxml model containing the given Scxml element
 	 * @param scxml element
 	 * @return integer representing the number of refinements needed
 	 */
-	public int getRefinementDepth(EObject scxmlElement) {
+	protected int getRefinementDepth(EObject scxmlElement) {
 		Integer depth = (Integer) storage.fetch("depth");
 		if (depth==null) {
 			IumlbScxmlAdapter adapter = new IumlbScxmlAdapter(null);
